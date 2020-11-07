43d0ad90a3784d8096cdbb776c6226302c4505cf
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-11-07 14:15:27 -0800

    implemented movement (without view angle change)

---
+++ Assets/Animators/PlayerMovementAux/MovementBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBehavior : StateMachineBehaviour
+{
+    // settings /////////////////////////////////////
+    public float backForthSpeed;
+    public float leftRightSpeed;
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // on each frame, change player speed according to 
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        Rigidbody rb = animator.gameObject.GetComponent<Rigidbody>();
+        Vector3 newVelocity = new Vector3(0, rb.velocity.y, 0);
+        newVelocity += (animator.GetBool("W")) ? animator.transform.forward * backForthSpeed : Vector3.zero;
+        newVelocity -= (animator.GetBool("S")) ? animator.transform.forward * backForthSpeed : Vector3.zero;
+        newVelocity += (animator.GetBool("A")) ? (Quaternion.Euler(0, -90, 0) * animator.transform.forward) * leftRightSpeed : Vector3.zero;
+        newVelocity += (animator.GetBool("D")) ? (Quaternion.Euler(0, 90, 0) * animator.transform.forward) * leftRightSpeed : Vector3.zero;
+
+        animator.gameObject.GetComponent<Rigidbody>().velocity = newVelocity;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Animators/PlayerMovementAux/MovementInput.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementInput : MonoBehaviour
+{
+    private Animator myAnimator;
+
+    // key settings ////////////////////////////////////////////////////////////////
+    public KeyCode forward = KeyCode.W;
+    public KeyCode backward = KeyCode.S;
+    public KeyCode left = KeyCode.A;
+    public KeyCode right = KeyCode.D;
+
+
+    // animator trigger collections ////////////////////////////////////////////////
+    private Dictionary<KeyCode, string> movementTriggers;
+
+
+    // system messages /////////////////////////////////////////////////////////////
+    void Start()
+    {
+        myAnimator = gameObject.GetComponent<Animator>();
+
+        // initialize movement trigger dictionary
+        movementTriggers = new Dictionary<KeyCode, string>();
+        movementTriggers.Add(forward, "W");
+        movementTriggers.Add(backward, "S");
+        movementTriggers.Add(left, "A");
+        movementTriggers.Add(right, "D");
+    }
+
+    void Update()
+    {
+        // update movement keys
+        ResetAnimator();
+        foreach (KeyCode i in CheckMovementInput()) {
+            myAnimator.SetBool(movementTriggers[i], true);
+            myAnimator.SetBool("MovePressed", true);
+        }
+
+        // TODO: update facing direction
+            // TODO: update horizontal movement
+            // TODO: update vertical movement
+    }
+
+
+    // aux functions ///////////////////////////////////////////////////////////////
+    private List<KeyCode> CheckMovementInput() { // check and return the pressed movement key
+        List<KeyCode> results = new List<KeyCode>();
+        results.Clear();
+        foreach (KeyCode i in new KeyCode[] { forward, backward, left, right }) {
+            if (Input.GetKey(i)) {
+                results.Add(i);
+            }
+        }
+
+        return results;
+    }
+
+    private void ResetAnimator() { // set all movement input bools false
+        foreach (string i in new string[] {"W", "S", "A", "D"}) {
+            myAnimator.SetBool(i, false);
+        }
+        myAnimator.SetBool("MovePressed", false);
+    }
+}
