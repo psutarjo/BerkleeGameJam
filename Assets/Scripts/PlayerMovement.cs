23f83208738c6ff138a8721876454f698b9efae6
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-11-07 11:19:16 -0800

    setting up teleporting functionality

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity.Set(0, rb.velocity.y, 0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            rb.velocity += transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            rb.velocity -= transform.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            rb.velocity += (Quaternion.Euler(0, -90, 0)*transform.forward) * speed;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            rb.velocity += (Quaternion.Euler(0, 90, 0) * transform.forward) * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/RespawnPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : MonoBehaviour
+{
+    // public /////////////////////////////////////////////////////////////////////////////
+    public GameObject respawnPoint;
+
+    // private ////////////////////////////////////////////////////////////////////////////
+
+
+    // trigger functions //////////////////////////////////////////////////////////////////
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag != "Player") {
+            return;
+        }
+
+        Debug.Log("collide");
+
+        Vector3 newPosition = other.gameObject.transform.position - transform.position
+            + respawnPoint.transform.position; // the position of player when sent back to respawn
+
+        Debug.Log(newPosition);
+
+        // teleport
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<Rigidbody>().MovePosition(newPosition);
+    }
+
+
+    // system messages ////////////////////////////////////////////////////////////////////
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
