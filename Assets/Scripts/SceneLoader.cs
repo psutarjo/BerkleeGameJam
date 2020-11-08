1d2e50c991e7616144565b7f03298612f917591b
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-11-08 02:18:08 -0800

    Implemented framework for scene loader and game manager.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader instance = null;
+    
+    // scenes marked by names ///////////////////////////////////////
+    public string gameScene;
+
+
+    // system message ///////////////////////////////////////////////
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+    // scenes ///////////////////////////////////////////////////////
+    public void StartGame() {
+        SceneManager.LoadScene(gameScene);
+    }
+}
