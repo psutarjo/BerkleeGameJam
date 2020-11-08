2b840e92ec8091d434e4ac6a4ab4027fec755df9
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-11-07 23:31:05 -0800

    Remade player controller using character controller component.

--- Assets/Scripts/ConrollerMovement.cs
+++ Assets/Scripts/ConrollerMovement.cs
@@ -7,10 +7,22 @@ public class ConrollerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+
+    Vector3 gVelocity;
+
+    public float g = 9.8f;
+
+    public Transform groundChecker;
+    public float groundBuffer = 0.4f;
+    public LayerMask groundMask;
+
+    private bool grounded;
     
     // Update is called once per frame
     void Update()
     {
+        grounded = Physics.CheckSphere(groundChecker.position, groundBuffer, groundMask);
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -18,5 +30,13 @@ public class ConrollerMovement : MonoBehaviour
         move *= speed * Time.deltaTime;
 
         controller.Move(move);
+        
+        gVelocity.y -= g * Time.deltaTime;
+        
+        if (grounded && gVelocity.y <= 0) {
+            gVelocity.y = -0.1f;
+        }
+        
+        controller.Move(gVelocity * Time.deltaTime);
     }
 }
--- Assets/Scripts/MouseMotion.cs
+++ Assets/Scripts/MouseMotion.cs
@@ -9,6 +9,10 @@ public class MouseMotion : MonoBehaviour
 
     public Transform playerBody;
 
+    public LayerMask runeLayer;
+
+    public float interactRange;
+
     private float xRotation = 0f;
 
     // Start is called before the first frame update
@@ -20,13 +24,37 @@ public class MouseMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xDiff = Input.GetAxis("Mouse X") * xSensitivity * Time.deltaTime;
-        float yDiff = Input.GetAxis("Mouse Y") * ySensitivity * Time.deltaTime;
+        float xDiff = Input.GetAxis("Mouse X") * xSensitivity;
+        float yDiff = Input.GetAxis("Mouse Y") * ySensitivity;
 
         xRotation -= yDiff;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * xDiff);
+
+        CheckClickInput();
+    }
+
+    private GameObject CheckClickInput() { // sending clicking and hovering messages
+        // I'm assuing there's only one object in contact with player
+        RaycastHit hit;
+        bool isClicking = Input.GetMouseButton(0);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactRange, runeLayer)) {
+            Clickable hovered = hit.collider.gameObject.GetComponent<Clickable>();
+            if (isClicking) {
+                hovered.Click();
+            }
+            else {
+                hovered.Hover();
+            }
+
+            return hit.collider.gameObject;
+        }
+        else {
+            Debug.Log("no hit");
+        }
+
+        return null;
     }
 }
