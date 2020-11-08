7346cca3c9805d1888f69bdfaafd37a4ede74541
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-11-07 17:36:43 -0800

    Implemented hover and clicking (template)

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Clickable : MonoBehaviour
+{
+    // interfaces /////////////////////////////////////////////////////
+    public abstract void Click();
+    public abstract void Hover();
+}
