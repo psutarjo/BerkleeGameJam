ed619d3dc3cc434d2c9c2077806dd61b5f1f02b9
Xiangyu Wan <johnnywon@g.ucla.edu>
2020-11-08 18:09:12 -0800

    Added end scene

---
+++ Assets/Scripts/Game Management/BackToMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class BackToMenu : MonoBehaviour
+{
+    public void Back() {
+        SceneLoader.instance.BackToMenu();
+    }
+}
